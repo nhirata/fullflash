9a2616d2caf6a72eaa3db289c8fe8309d2640916
nhirata <nhirata.bugzilla@gmail.com>
2014-01-21 02:28:41 -0500

    Profile backup/restore

--- fullflash_gecko_ril_gaia.sh
+++ fullflash_gecko_ril_gaia.sh
@@ -1,13 +1,132 @@
 #!/bin/bash
-
+Shallow_Flag=1
+Backup_Flag=0
+Recover_Flag=0
 rildebug=0
 nocomril=0
 keepdata=0
 forcetosystem=0
 installedonsystem=0
+specificdevice=0
 
+function helper(){
+    echo -e "
+    -r : to turn on ril debugging
+    -n : to not install comril
+    -f : force install to system partition
+    -h : for help
+    -i : image flash
+    -s : flash to specific device serial
+    -b : backup only
+    -r : restore only
+    -k : keep previous profile; backup and restore
+    "
+    exit 0
+}
+
+function backup(){
+    if [ ! -d mozilla-profile ]; then
+        echo "no backup folder, creating..."
+        mkdir mozilla-profile
+    fi
+    echo -e "Backup your profiles..."
+    adb shell stop b2g 2> ./mozilla-profile/backup.log &&\
+    rm -rf ./mozilla-profile/* &&\
+    mkdir -p mozilla-profile/profile &&\
+    adb pull /data/b2g/mozilla ./mozilla-profile/profile 2> ./mozilla-profile/backup.log &&\
+    mkdir -p mozilla-profile/data-local &&\
+    adb pull /data/local ./mozilla-profile/data-local 2> ./mozilla-profile/backup.log &&\
+    rm -rf mozilla-profile/data-local/webapps
+    adb shell start b2g 2> ./mozilla-profile/backup.log
+    echo -e "Backup done."
+    exit 0
+}
+
+function restore(){
+    echo -e "Recover your profiles..."
+    if [ ! -d mozilla-profile/profile ] || [ ! -d mozilla-profile/data-local ]; then
+        echo "no recover files."
+        exit -1
+    fi
+    adb shell stop b2g 2> ./mozilla-profile/recover.log &&\
+    adb shell rm -r /data/b2g/mozilla 2> ./mozilla-profile/recover.log &&\
+    adb push ./mozilla-profile/profile /data/b2g/mozilla 2> ./mozilla-profile/recover.log &&\
+    adb push ./mozilla-profile/data-local /data/local 2> ./mozilla-profile/recover.log &&\
+    adb reboot
+    sleep 30
+    echo -e "Recover done."
+    exit 0
+}
+
+function run_adb()
+{
+    # TODO: Bug 875534 - Unable to direct ADB forward command to inari devices due to colon (:) in serial ID
+    # If there is colon in serial number, this script will have some warning message.
+	adb $ADB_FLAGS $@
+}
+
+function flash_gecko() {
+    run_adb root
+    run_adb wait-for-device &&
+    run_adb remount
+    run_adb wait-for-device &&
+    echo + Check how much space is taken &&
+    run_adb shell df /system &&
+    echo + removing old system &&
+    run_adb shell rm -r /system/b2g &&
+    echo + Check how much is removed afterwards &&
+    adb shell df /system &&
+    run_adb push b2g /system/b2g &&
+    echo + Check how much is placed on after system install&&
+    adb shell df /system &&
+}
+function flash_comril() {
+  echo + Installing new RIL &&
+  run_adb push ril /system/b2g/distribution/bundles/
+  echo + Done installing RIL!
+}
+
+function adb_clean_gaia() {
+    echo "Clean Gaia and profiles ..."
+    echo + Deleting Profile data &&
+    run_adb shell rm -r /data/b2g/* &&
+    run_adb shell rm -r /data/local/user.js &&
+    run_adb shell rm -r /data/local/indexedDB &&
+    run_adb shell rm -r /data/local/debug_info_trigger &&
+    run_adb shell rm -r /data/local/storage/persistent/* &&
+    run_adb shell rm -r /data/local/permissions.sqlite* &&
+    echo + Deleting any old gaia and cache &&
+    run_adb shell rm -r /system/b2g/webapps &&
+    run_adb shell rm -r /data/local/webapps &&
+    run_adb shell rm -r /data/local/svoperapps &&
+    run_adb shell rm -r /data/local/OfflineCache &&
+    run_adb shell rm -r /cache/* &&
+    echo "Clean Done."
+}
+
+function adb_push_gaia() {
+    GAIA_DIR=$1
+    
+    ## Adjusting user.js
+    cat gaia/profile/user.js | sed -e "s/user_pref/pref/" > user.js
+    
+    echo "Push Gaia ..."
+    run_adb shell mkdir -p $GAIA_DIR/system/b2g/defaults/pref &&
+    run_adb push gaia/profile/webapps $GAIA_DIR/webapps &&
+    run_adb push user.js $GAIA_DIR/defaults/pref &&
+    run_adb push gaia/profile/settings.json $GAIA_DIR/defaults &&
+    echo "Push Done."
+}
+
+function flash_gaia() {
+    adb_clean_gaia &&
+    adb_push_gaia
+}
+
+## Switches
 while getopts :rnh opt; do
   case $opt in
+    b) Backup_Flag=1; 
     r)
     echo "Turning on RIL Debugging"
     rildebug=1
@@ -16,63 +135,36 @@ while getopts :rnh opt; do
     echo "Not installing Commercial Ril, should be using mozril"
     nocomril=1
     ;;
-    s) 
+    s)
+      case "$2" in
+        "") shift 2;;
+        *) ADB_DEVICE=$2; ADB_FLAGS+="-s $2"; shift 2;;
+      esac ;;
+      specificdevice=$2
+    f) 
     echo "Force install to system"
     forcetosystem=1 
     ;;
-    h) 
-    echo "
-    -k : keep previous profile
-    -r : to turn on ril debugging
-    -n : to not install comril
-    -s : force install to system partition
-    -h : for help
-    "
-    exit
+    h|help) helper;
+    exit 0
     ;;
     k)
     echo "keep previous Profile"
-    keepdata=1
+    Backup_Flag=1
+    Recover_Flag=1
     ;;
     *)
     ;;
   esac
 done
 
-echo + gaining root access &&
-adb root &&
-
-echo + Waiting for adb to come back up &&
-adb wait-for-device &&
-
-echo + remounting the system partition &&
-adb remount &&
-adb shell mount -o remount,rw /system &&
-
-echo + Waiting for adb to come back up &&
-adb wait-for-device &&
-
-echo + Stopping b2g &&
-adb shell stop b2g &&
-
-echo + Check how much space is taken &&
-adb shell df /system &&
-
-echo + removing old system &&
-adb shell rm -r /system/b2g &&
-
-echo + Check how much is removed afterwards &&
-adb shell df /system &&
-
-echo + Installing new b2g &&
-adb push b2g /system/b2g &&
-
-echo + Done installing Gecko! &&
+#Backup
+if [ $Backup_Flag ]; then
+  backup
+fi
 
 if [ ! $nocomril ] ; then
-  echo + Installing new RIL &&
-  adb push ril /system/b2g/distribution/bundles/
-  echo + Done installing RIL!
+  flash_comril
 else
   echo + COM RIL not installed
 fi
@@ -98,42 +190,22 @@ fi
 
 echo "installedonsystem = ${installedonsystem}" &&
 
-echo + Deleting any old gaia and cache &&
-adb shell rm -r /cache/* &&
-adb shell rm -r /data/local/webapps &&
-adb shell rm -r /data/local/svoperapps &&
-adb shell rm -r /data/local/OfflineCache &&
-adb shell rm -r /system/b2g/webapps &&
-
-if [ $keepdata ] ; then
-  echo + Deleting Profile data &&
-  adb shell rm -r /data/b2g/* &&
-  adb shell rm -r /data/local/storage/persistent/*
-  adb shell rm -r /data/local/user.js &&
-  adb shell rm -r /data/local/permissions.sqlite* &&
-  adb shell rm -r /data/local/indexedDB &&
-  adb shell rm -r /data/local/debug_info_trigger
-else
-  echo + keeping data profile
-fi
-
 echo + Installing new gaia webapps &&
 if [ $forcetosystem -o $installedonsystem ] ; then
   echo + installing to system
-  echo "force to system : $forcetosystem ; installed onsystem : $installedonsystem"
-  adb shell mkdir -p /system/b2g/defaults/pref &&
-  adb push gaia/profile/webapps /system/b2g/webapps
-  adb push user.js /system/b2g/defaults/pref &&
-  adb push gaia/profile/settings.json /system/b2g/defaults
+  $install_directory="/system/b2g"
 else
   echo + installing to data/local
-  echo "force to system : $forcetosystem ; installed onsystem : $installedonsyste
-m"
-  adb shell mkdir -p /system/b2g/defaults/pref &&
-  adb push user.js /system/b2g/defaults/pref &&
-  adb push gaia/profile/webapps /data/local/webapps
-  adb push gaia/profile/settings.json /system/b2g/defaults 
+    $install_directory="/data/local"
 fi
+
+adb_push_gaia $install_directory
+
+#Restore
+if [ $RecoverOnly_Flag == true ]; then
+  restore
+fi
+
 echo + Rebooting &&
 adb shell sync &&
 adb shell reboot &&

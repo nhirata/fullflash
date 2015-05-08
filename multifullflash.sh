302db351f44a4cd71700d61e171f196c5e70af25
nhirata <nhirata.bugzilla@gmail.com>
2015-05-07 20:59:15 -0700

    fixes to the multifullflash. Pathing was an issue. Also fixed readme on notes for how to use multifullflash.sh

--- multifullflash.sh
+++ multifullflash.sh
@@ -4,5 +4,6 @@ List=`fastboot devices | grep -v "List" | awk '{print $1}'`
 for DEVICE in ${List}
 do 
   echo "flashing ${DEVICE}"
-  ./b2g-distro/flash.sh -s ${DEVICE} 
+  ./flash.sh -s ${DEVICE} 
 done
+
